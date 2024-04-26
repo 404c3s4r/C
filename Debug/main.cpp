dd690b8c4b6f807620b82f5f05801406ad74cf6f
Roberto Oliveira <robertocoliver@protonmail.com>
2024-04-26 18:40:54 -0300

    Debug and documentation

---
+++ Debug/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	printf("Estou debugando o meu código\n");
+	printf("Estou debugando o meu código\n");
+	printf("Estou debugando o meu código\n");
+	printf("Estou debugando o meu código\n");
+	printf("Estou debugando o meu código\n");
+}
+
+
+/* 
+Por quê debugar o código ? 
+Pode acontecer do nosso código não está errado em termos de sintaxe, se estiver, o compilador vai nos retornar o erro.
+Entretanto, pode acontecer da lógica do nosso programa está errada, retornando um comportamento que não era esperado inicialmente.
+Logo, para depurar os erros de lógica, se faz necessário debugar o código linha por linha observando a resposta de retorno.
+
+
+
+
+
+
+*/
---
+++ M00E02/003.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	printf('Hello world\n'); //Ao trabalharmos com C, devemos usar aspas duplas ""; Erro ao utilizar '' ( aspas simples ):  [Warning] character constant too long for its type
+	system("pause");
+    
+}
+/*
+Este é um comentário multilinhas. 
+Comentários com várias linhas começam com /* e terminam com * / 
+*/ 
