bcd626d1d7777a30b58538a4c112e78d25d3ab29
Roberto Oliveira <robertocoliver@protonmail.com>
2024-05-12 01:50:38 -0300

    Numeric Types

---
+++ M00E012/017.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+// ASCII --> https://www.w3schools.com/charsets/ref_html_ascii.asp
+main(){
+	char a = 36; 
+	printf("%c", a);
+	return 0; 
+
+}//The output will be a dollar sign.
+
+
+
+// In C, we don't use char to handle or store characters; instead, we use arrays of characters or pointers (strings) provided by the standard C library functions. 
+//With these functions, we can allocate memory for strings
