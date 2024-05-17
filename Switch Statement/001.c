40b069e45f18d116c4ff3d3000bd422424f0899c
Roberto Oliveira <robertocoliver@protonmail.com>
2024-05-16 23:02:28 -0300

    Temperature Converter

--- Switch Statement/001.c
+++ Switch Statement/001.c
@@ -1,65 +1,45 @@
-// Escreva um programa em C que converte uma temperatura de Celsius para Fahrenheit ou de Fahrenheit para Celsius, dependendo da escolha do usuário. 
-// O programa deve perguntar ao usuário qual conversão ele deseja fazer e então solicitar a temperatura correspondente. 
+// Write a program in C that converts a temperature from Celsius to Fahrenheit or from Fahrenheit to Celsius, depending on the user's choice.
+// The program should ask the user which conversion he wants to perform and then request the corresponding temperature.
 #include <stdio.h> 
 
-#define texto "Conversor de temperatura"
+#define text "Temperature Converter"
 
-// Primeiro vamos fazer de celcius para f 
+// First let's do from Celsius to Fahrenheit
 
-float celsiusforfahrenheit(float celsius){
-	return 1.8 * celsius + 32;
+float celsiusToFahrenheit(float celsius) {
+    return 1.8 * celsius + 32;
 }
 
-float fahrenheitforcelsius(float fahrenheit){
-	return 	(fahrenheit - 32) / 1.8;
+float fahrenheitToCelsius(float fahrenheit) {
+    return (fahrenheit - 32) / 1.8;
 }
 
+int main() {
+    int choice;
+    float celsius, fahrenheit, result;
+    
+    printf("%s\n", text);
+    printf("Enter:\n1 Celsius to Fahrenheit\n2 Fahrenheit to Celsius\n");   
+    scanf("%d", &choice);
+    
+    switch(choice) {
+        case 1:
+            printf("Enter a value in Celsius: ");
+            scanf("%f", &celsius);
+            result = celsiusToFahrenheit(celsius);
+            printf("%.1f F", result);
+            break;
+        
+        case 2:
+            printf("Enter a value in Fahrenheit: ");
+            scanf("%f", &fahrenheit);
+            result = fahrenheitToCelsius(fahrenheit);
+            printf("%.1f C", result);
+            break;
+        default:
+            printf("Invalid option");
+            break;
+    }
 
-int main(){
-	int choice;
-	float celsius, fahrenheit, result;
-	
-	printf("%s\n", texto);
-	printf("Digite:\n1 Celsius para fahrenheit\n2 Fahrenheit para celsius\n");	
-	scanf("%d", &choice);
-	
-	
-	switch(choice) {
-		
-		case 1:
-			printf("Digite um valor em Celsius\n");
-			scanf("%f", &celsius);
-			result = celsiusforfahrenheit(celsius);
-			printf("%.1f F", result);
-			break;
-		
-		case 2:
-			printf("Digite um valor em fahrenheit\n");
-			scanf("%f", &fahrenheit);
-			result = fahrenheitforcelsius(fahrenheit);
-			printf("%.1f C", result);
-			break;
-	default:
-		printf("Opcao invalida");
-		break;
-	
-	}
-
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-/* 
-> Case label does not reduce to an integer constant. 
-> case == 2 É UMA EXPRESSÃO E TA ERRADO. ( devem ser inteiros constantes ) 
-> case 2: É uma expressão onde o 2 é inteiro e constante. 
-
-
-
-*/ 
