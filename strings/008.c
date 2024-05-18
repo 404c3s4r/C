85960878ff6198b5969a2f5ee79b857ed90adb0e
Roberto Oliveira <robertocoliver@protonmail.com>
2024-05-18 17:44:08 -0300

    scanf limites de uso
    Scanf considera os espaços em branco como terminação de string

---
+++ strings/008.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){
+	char nome[64];
+	
+	printf("Digite seu nome completo:\n ");
+	scanf("%s", &nome);
+	printf("Prazer, %s", nome);
+	// Output Roberto 
+	//scanf considera os espa em branco como uma terminacao de strings 
+} 
+
