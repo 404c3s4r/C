8bc8f69b6c0a43697ce1d7606d71fbaca1e47ae1
Roberto Oliveira <robertocoliver@protonmail.com>
2024-05-18 14:13:00 -0300

    Access Strings

---
+++ strings/002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// %s strings 
+// %c single character 	
+int main(){
+	
+	char ola[] = "Hello World!";
+		printf("%s", ola); // if you use %s, the interpreter considers ola[i] as a single character
+
+}
+
+	
+
