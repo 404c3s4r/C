23f0fe21ee09ebb82bce28aab113aa5811d33146
Roberto Oliveira <robertocoliver@protonmail.com>
2024-05-22 23:08:42 -0300

    Update 006.c

--- Functions/006.c
+++ Functions/006.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-void myfunction(char* fullname){
+void myfunction(char* fullname){ // Estamos passando o endereco da memoria no primeiro caractere 'R'. fullname e um ponteiro para o primeiro caractere R 
 
 	printf("Nome: %s", fullname);
 
@@ -12,14 +12,14 @@ void myfunction(char* fullname){
 
 int main(){
 
-	char fullname[64];
+	char fullname[64]; // Criado o array aqui
 	size_t size; 
 	do {
 	
 		printf("Cadastre o nome de usuario aqui: ");
 		fgets(fullname, sizeof(fullname), stdin);
 	
-		size = sizeof(fullname) / sizeof(fullname[0]);
+		size = sizeof(fullname) / sizeof(fullname[0]);// Lembrar que tem o strlen tambem 
 		
 		
 	}while(size < 1 || (size == 1 && fullname[0] == '\n')); // Caractere sozinho, sempre com ' ' ( aspas simples ) 
@@ -28,3 +28,5 @@ int main(){
 	myfunction(fullname);	
 	return 0;
 }
+
+ // Exemplo: Roberto 'R', 'O', 'B', 'E', 'R', 'T', 'O', '\n', '\0' Logo, se toda string e um array, precisamos passar o char* para passar o primeiro
