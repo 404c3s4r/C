85dc81e19b704397f479fa3f98dae54d5bc6dc84
Roberto Oliveira <robertocoliver@protonmail.com>
2024-05-26 18:56:22 -0300

    strings versus characters

--- Introduction/Line Counting/002.c
+++ Introduction/Line Counting/002.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    char nome  = 'A';
-    int result = nome - 1;  
-    printf("Tamanho de result em bytes: %lu\n", sizeof(result)); 
+    char nome  = 'A'; // A character written between single quotes represents an integer value equal to the numerical
+    int result = nome - 1;  // 'A' is a character; in ASCII character set its value 65 - 1 = 64 or (@) 
+    printf("Result: %d\n", sizeof(result)); 
 
-    return 0;
+    return 0; 
 }
 
---
+++ Introduction/Line Counting/003.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	char n1 = '\n'; 
+	printf("%d", n1); 
+	// '\n' is 10 in ASCII // "\n" is a string constant 
+}
