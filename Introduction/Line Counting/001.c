b8b0bf32e76f63acce2ba8002f52dbfb575473a8
Roberto Oliveira <robertocoliver@protonmail.com>
2024-05-26 16:25:30 -0300

    Line Counting

---
+++ Introduction/Line Counting/001.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h> 
+
+/*
+int main(){ 
+    int c, n1; 
+    n1 = 0;  // Adicionei o ponto-e-vírgula que faltava aqui
+    while ((c = getchar()) != EOF){
+        if(c == '\n')
+            ++n1; 
+        printf("%d\n", n1);
+    }
+    return 0;  // Adicionei a declaração de retorno para indicar o final da função main
+}
+*/ 
+
+/* 
+int main() {
+    char string[] = "helloworld";
+    // int length = strlen(string);
+    int lenght = sizeof(string) / sizeof(string[0]);
+
+    printf("A string %d", lenght);
+
+    return 0;
+} 
+// // Returns 11 characters: 'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'. If we used the function "strlen", we would receive "10 characters".
+
+*/
+
+
+
+
+
