7839b1cd0cee52a7cfa408a375e26fa8cd843918
Roberto Oliveira <robertocoliver@protonmail.com>
2025-03-29 14:09:25 -0300

    .

--- Introduction/File copying/001.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> 
-
-int main(){
-	char c; 
-	printf("Write anything: "); 
-	c = getchar(); // That copies its input to its output one character at a time. 
-	printf("Output: %c", c);
-
-}
--- Introduction/Line Counting/003.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	char n1 = '\n'; 
-	printf("%d", n1); 
-	// '\n' is 10 in ASCII // "\n" is a string constant 
-}
